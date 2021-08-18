907059a78e0c2a486ab0a200ba74818031c4be7e
govindkumawat <kumawatgoivnd1997@gmail.com>
2021-08-18 22:50:01 +0530

    first commit

---
+++ src/Header.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export const Header = () => {
+    return (
+        <div>
+            <h1 className='text-center bg-light'>Kanban Board</h1>
+        </div>
+    )
+}
--- src/index.js
+++ src/index.js
@@ -1,17 +1,153 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React, { useState } from "react";
+import ReactDOM from "react-dom";
+import Board, { moveCard } from "@asseinfo/react-kanban";
+import "@asseinfo/react-kanban/dist/styles.css";
+import { Header } from "./Header";
+import './index.css'
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+const board = {
+  columns: [
+    {
+      id: 1,
+      title: "Assigned",
+      cards: [
+        {
+          id: 1,
+          title: "Card title 1",
+          description: "Card content"
+        },
+        {
+          id: 2,
+          title: "Card title 2",
+          description: "Card content"
+        },
+        {
+          id: 3,
+          title: "Card title 3",
+          description: "Card content"
+        }
+      ]
+    },
+    {
+      id: 2,
+      title: "Ongoing",
+      cards: [
+        {
+          id: 9,
+          title: "Card title 9",
+          description: "Card content"
+        }
+      ]
+    },
+    {
+      id: 3,
+      title: "To be tested",
+      cards: [
+        {
+          id: 10,
+          title: "Card title 10",
+          description: "Card content"
+        },
+        {
+          id: 11,
+          title: "Card title 11",
+          description: "Card content"
+        }
+      ]
+    },
+    {
+      id: 4,
+      title: "Completed",
+      cards: [
+        {
+          id: 12,
+          title: "Card title 12",
+          description: "Card content"
+        },
+        {
+          id: 13,
+          title: "Card title 13",
+          description: "Card content"
+        }
+      ]
+    }
+  ]
+};
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+function ControlledBoard() {
+  // You need to control the state yourself.
+  const [controlledBoard, setBoard] = useState(board);
+
+  function handleCardMove(_card, source, destination) {
+    const updatedBoard = moveCard(controlledBoard, source, destination);
+    setBoard(updatedBoard);
+  }
+
+  // Note: To enable user to create a new column all the following are needed as a minimum
+
+  /** This is called by onColumnNew on the board when a new column has been requested and 
+  the form on the ColumnAdder component has been filled in by the user. We update our state
+  to get the board to render with the new data */
+  const handleColumnAdded = (newBoard, newColumn) => {
+    console.info("column added!");
+    setBoard(newBoard);
+  };
+
+  /** This is called when a new column has been requested, its main job is 
+   to give you a chance to save it and return a new id for the column */
+  const handleColumnConfirmed = (newColumnName) => {
+    console.info("Column id requested");
+    // You will need to generate a new id for you column here - id from a database insert or similar?
+    const newColumn = {
+      id: "testing-but-this-should-be-unique",
+      ...newColumnName
+    };
+    return newColumn;
+  };
+
+  return (
+    <Board
+      onCardDragEnd={handleCardMove}
+      disableColumnDrag
+      allowAddColumn={true}
+      onNewColumnConfirm={handleColumnConfirmed}
+      onColumnNew={handleColumnAdded}
+    >
+      {controlledBoard}
+    </Board>
+  );
+}
+
+function UncontrolledBoard() {
+  return (
+    <Board
+      allowRemoveLane
+      allowRenameColumn
+      allowRemoveCard
+      onLaneRemove={console.log}
+      onCardRemove={console.log}
+      onLaneRename={console.log}
+      initialBoard={board}
+      allowAddCard={{ on: "top" }}
+      onNewCardConfirm={(draftCard) => ({
+        id: new Date().getTime(),
+        ...draftCard
+      })}
+      onCardNew={console.log}
+    />
+  );
+}
+
+function App() {
+  return (
+    <>
+      <Header/>
+      <UncontrolledBoard />
+      
+      
+    </>
+  );
+}
+
+const rootElement = document.getElementById("root");
+ReactDOM.render(<App />, rootElement);
